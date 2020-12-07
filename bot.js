906c89c159f6f5b8a62fbe41f39b0094fa3b3d44
Fuuka <ajioe1111@gmail.com>
2020-12-08 01:46:29 +0300

    Сокращение команды !add

--- bot.js
+++ bot.js
@@ -3,11 +3,38 @@ const client = new Discord.Client();
 const fs = require('fs');
 let config = require('./config.json');
 let prefix = config.prefix;
-let waifuArr = ['Megumin', 'Rem']
+
+let waifuArr = ["Megumin", "Rem"];
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
+
+client.on(`message`, msg => {
+  if (msg.content.startsWith(prefix + `add`)) {
+    let args = msg.content.split(' ');
+    args.shift();
+    args = args[0].split(';');
+    if (args[0] == undefined || args[1] == undefined || args[2] == undefined || args[3] == undefined || args[4] == undefined) {
+      (msg.reply(`Ошибка ввода! Пример команды: !add Name;OriginalName;Age;DateOfBirth;Description`));
+    }
+    else {
+      let name = args[0];
+      waifuArr.push(name);
+      let original = args[1];
+      let waifuAge = args[2];
+      let date = args[3];
+      let desc = args[4];
+      let waifuId = waifuArr.length - 1;
+      let waifu = { fullName: name, id: waifuId, originalName: original, age: waifuAge, dateOfBirth: date, description: desc };
+      fs.writeFileSync(`./waifu/${waifuId}.json`, JSON.stringify(waifu));
+      msg.reply(`waifu добавлена!`);
+    }
+  }
+})
+
+
 
 client.on(`message`, msg => {
   if (msg.content === prefix + `list`) {
@@ -29,7 +56,7 @@ client.on(`message`, (msg) => {
       msg.reply(`Информация о Waifu`);
       msg.channel.send(`Полное имя: ${waifu.fullName}\r\n Оригинальное имя: ${waifu.originalName}\r\n Возраст: ${waifu.age}\r\n День рождения: ${waifu.dateOfBirth}\r\n \r\n Описание: ${waifu.description}\r\n \r\n ID: ${waifu.id}`);
     }
-    else {msg.reply(`Нету waifu под таким ID ;(`)}
+    else { msg.reply(`Нету waifu под таким ID ;(`) }
   }
 });
 
